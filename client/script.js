bfbc5d57a689596886ae367f6d0a1c374a754032
ugo@picklesfamily.com <ugo.mare.epitech@gmail.com>
2014-11-09 12:49:58 +0100

    client

--- client/script.js
+++ client/script.js
@@ -2,6 +2,19 @@ var hoMyGodeApp = angular.module('hoMyGodeApp', ['ngRoute']);
 
 ///Routes
 
+// $products = [
+// 	{id: 1,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 2,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 3,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 4,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 5,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 6,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 7,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 8,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 9,  name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"},
+// 	{id: 10, name: "produit1", descr: "Ceci est un produit", category: "cat", prix: "10", seller: "seller1", thumb "images/thumb.png", created_at: Date.new, phone_number:" 0622334455"}
+// ];
+
 hoMyGodeApp.config(function($routeProvider) {
 	$routeProvider
 
@@ -26,7 +39,7 @@ hoMyGodeApp.config(function($routeProvider) {
 		})
 		
 		.otherwise({redirectTo:'/'})
-		
+
 		;
 });
 
@@ -40,7 +53,18 @@ hoMyGodeApp.controller('mainController', function($scope, $route, $routeParams){
 
 
 hoMyGodeApp.controller('listController', function($scope){
-	
+	$scope.products = [
+		{id: 1,  name: "produit1",  descr: "Ceci est un produit", category: "cat1",  price: "10", seller: "seller1",  location: "location1",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 2,  name: "produit2",  descr: "Ceci est un produit", category: "cat2",  price: "10", seller: "seller2",  location: "location2",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 3,  name: "produit3",  descr: "Ceci est un produit", category: "cat3",  price: "10", seller: "seller3",  location: "location3",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 4,  name: "produit4",  descr: "Ceci est un produit", category: "cat4",  price: "10", seller: "seller4",  location: "location4",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 5,  name: "produit5",  descr: "Ceci est un produit", category: "cat5",  price: "10", seller: "seller5",  location: "location5",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 6,  name: "produit6",  descr: "Ceci est un produit", category: "cat6",  price: "10", seller: "seller6",  location: "location6",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 7,  name: "produit7",  descr: "Ceci est un produit", category: "cat7",  price: "10", seller: "seller7",  location: "location7",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 8,  name: "produit8",  descr: "Ceci est un produit", category: "cat8",  price: "10", seller: "seller8",  location: "location8",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 9,  name: "produit9",  descr: "Ceci est un produit", category: "cat9",  price: "10", seller: "seller9",  location: "location9",  thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 10, name: "produit10", descr: "Ceci est un produit", category: "cat10", price: "10", seller: "seller10", location: "location10", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"}
+	];
 });
 
 hoMyGodeApp.controller('loginController', function($scope){
