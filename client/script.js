ea4bba3d0292823072500e2a7f082e86b9ee6963
UgoMare <ugo.mare.epitech@gmail.com>
2014-12-26 03:56:45 +0100

    ajout des cat + regions

--- client/script.js
+++ client/script.js
@@ -28,14 +28,14 @@ hoMyGodeApp.config(function($routeProvider) {
 
 		//product
 
-		.when('/product/:productId', {
+		.when('/product/:id', {
 			templateUrl: 'pages/product.html',
 			controller: 'productController'
 		})
 
 		//product message
 
-		.when('/product/:productId/message', {
+		.when('/product/:id/message', {
 			templateUrl: 'pages/send_message.html',
 			controller: 'productMessageController'
 		})
@@ -44,7 +44,7 @@ hoMyGodeApp.config(function($routeProvider) {
 
 		//poduct edit
 
-		.when('/product/:productId/edit', {
+		.when('/product/:id/edit', {
 			templateUrl: 'pages/edit_product.html',
 			controller: 'productEditController'
 		})
@@ -61,6 +61,64 @@ hoMyGodeApp.config(function($routeProvider) {
 		;
 });
 
+products = [
+		{id: 1,  name: "produit1",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller1",  region: "Auvergne", location: "location1",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 2,  name: "produit2",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller2",  region: "Auvergne", location: "location2",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 3,  name: "produit3",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller3",  region: "Auvergne", location: "location3",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 4,  name: "produit4",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller4",  region: "Auvergne", location: "location4",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 5,  name: "produit5",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller5",  region: "Auvergne", location: "location5",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 6,  name: "produit6",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller6",  region: "Auvergne", location: "location6",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 7,  name: "produit7",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller7",  region: "Auvergne", location: "location7",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 8,  name: "produit8",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller8",  region: "Auvergne", location: "location8",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 9,  name: "produit9",  descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller9",  region: "Auvergne", location: "location9",  zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
+		{id: 10, name: "produit10", descr: "Ceci est un produit", category: "Canard Vibrant", price: "10", seller: "seller10", region: "Auvergne", location: "location10", zip_code: "12345", img1: "images/thumb.png", img2: "images/thumb.png", img3: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"}
+	];
+
+regions = [
+{ name: "Alsace" },
+{ name: "Aquitaine" },
+{ name: "Auvergne" },
+{ name: "Basse-Normandie" },
+{ name: "Bourgogne" },
+{ name: "Bretagne" },
+{ name: "Centre" },
+{ name: "Champagne-Ardenne" },
+{ name: "Corse" },
+{ name: "Franche-Comté" },
+{ name: "Haute-Normandie" },
+{ name: "Île-de-France" },
+{ name: "Languedoc-Roussillon" },
+{ name: "Limousin" },
+{ name: "Lorraine" },
+{ name: "Midi-Pyrénées" },
+{ name: "Nord-Pas-de-Calais" },
+{ name: "Pays de la Loire" },
+{ name: "Picardie" },
+{ name: "Poitou-Charentes" },
+{ name: "Provence-Alpes-Côte d'Azur" },
+{ name: "Rhône-Alpes"}
+];
+
+categories = [
+{ name: "Boules de Geïsha" },
+{ name: "Canard Vibrant" },
+{ name: "Oeufs Vibrants" },
+{ name: "Sextoy Rabbit" },
+{ name: "Coffrets & Accessoires" },
+{ name: "Gode & Godemichet" },
+{ name: "Gode Ceinture" },
+{ name: "Machine de l'Amour" },
+{ name: "Sextoys Anal" },
+{ name: "Stimulateurs Clitoridiens" },
+{ name: "Vibromasseurs" },
+{ name: "Masturbateur Homme" },
+{ name: "Poupée Gonflabe" },
+{ name: "Stimulateur Prostatique" },
+{ name: "Anneaux & Gaines de Pénis" },
+{ name: "Gode & Plug" },
+{ name: "Gode Ceinture Homme" }
+];
+
 hoMyGodeApp.controller('mainController', function($scope, $route, $routeParams){
 	
 	$scope.params = $routeParams;
@@ -69,31 +127,38 @@ hoMyGodeApp.controller('mainController', function($scope, $route, $routeParams){
 
 hoMyGodeApp.controller('productsController', function($scope){
 
-	$scope.products = [
-		{id: 1,  name: "produit1",  descr: "Ceci est un produit", category: "cat1",  price: "10", seller: "seller1",  region: "region1",  location: "location1",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 2,  name: "produit2",  descr: "Ceci est un produit", category: "cat2",  price: "10", seller: "seller2",  region: "region2",  location: "location2",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 3,  name: "produit3",  descr: "Ceci est un produit", category: "cat3",  price: "10", seller: "seller3",  region: "region3",  location: "location3",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 4,  name: "produit4",  descr: "Ceci est un produit", category: "cat4",  price: "10", seller: "seller4",  region: "region4",  location: "location4",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 5,  name: "produit5",  descr: "Ceci est un produit", category: "cat5",  price: "10", seller: "seller5",  region: "region5",  location: "location5",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 6,  name: "produit6",  descr: "Ceci est un produit", category: "cat6",  price: "10", seller: "seller6",  region: "region6",  location: "location6",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 7,  name: "produit7",  descr: "Ceci est un produit", category: "cat7",  price: "10", seller: "seller7",  region: "region7",  location: "location7",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 8,  name: "produit8",  descr: "Ceci est un produit", category: "cat8",  price: "10", seller: "seller8",  region: "region8",  location: "location8",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 9,  name: "produit9",  descr: "Ceci est un produit", category: "cat9",  price: "10", seller: "seller9",  region: "region9",  location: "location9",  zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"},
-		{id: 10, name: "produit10", descr: "Ceci est un produit", category: "cat10", price: "10", seller: "seller10", region: "region10", location: "location10", zip_code: "12345", thumb: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"}
-	];
+	$scope.products = products;
+	$scope.regions = regions;
+	$scope.categories = categories;
 });
 
-hoMyGodeApp.controller('productController', function($scope){
-
-	$scope.product = {id: 1,  name: "produit1",  descr: "Ceci est un produit", category: "cat1",  price: "10", seller: "seller1",  location: "location1", zip_code: "12345", img: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"};
+hoMyGodeApp.controller('productController', function($scope, $routeParams){
+	
+	var id = $routeParams.id;
+	$scope.product = products[id-1];
 });
 
-hoMyGodeApp.controller('productMessageController', function($scope){
+hoMyGodeApp.controller('productMessageController', function($scope, $routeParams){
 
-	$scope.product = {id: 1,  name: "produit1", category: "cat1",  price: "10", seller: "seller1",  location: "location1", zip_code: "12345", img: "images/thumb.png", created_at: new Date(), phone_number:"0622334455"};
+	var id = $routeParams.id;
+	$scope.product = products[id-1];
 });
 
 hoMyGodeApp.controller('productNewController', function($scope){
+
+$scope.uploadFile = function(files) {
+    var fd = new FormData();
+ 
+    fd.append("file", files[0]);
+
+    // $http.post("uploadUrl", fd, {
+    //     withCredentials: true,
+    //     headers: {'Content-Type': undefined },
+    //     transformRequest: angular.identity
+    // }).success( ...all right!... ).error( ..damn!... );	
+
+};
+
 
 	$scope.save = function save(product) {
 		if (product != undefined 
@@ -110,9 +175,12 @@ hoMyGodeApp.controller('productNewController', function($scope){
 	}
 }});
 
-hoMyGodeApp.controller('productEditController', function($scope){
+hoMyGodeApp.controller('productEditController', function($scope, $routeParams){
 
-	$scope.product = {id: 1,  name: "produit1", descr: "Ceci est un produit", category: "cat1",  price: "10", seller: "seller1",  location: "location1", zip_code: "12345", img: "images/thumb.png", created_at: new Date(), phone_number:"0622334455", region: "region1"};
+	var id = $routeParams.id;
+	$scope.product = products[id-1];
+	$scope.regions = regions;
+	$scope.categories = categories;
 
 
     //     var id = $routeParams.id;
